bd550daca607b80be66ac022847c45ff1ba08e9c
Mike Tamis <tamis.mike@gmail.com>
2018-11-08 20:18:58 +1300

    refactored the extension

---
+++ renderGenericTemplate.js
@@ -0,0 +1,12 @@
+
+function renderGenericTemplate(code, propsPromise) {
+    return Promise.resolve(propsPromise).then((props) => {
+        let output = code;
+        Object.entries(props).forEach(([key, value]) => {
+            output =  output.replace(new RegExp("\\$\\{" + key.replace('$', '\\$') + "\\}", "g"), value)
+        }) 
+        return output;
+    })
+}
+
+module.exports = renderGenericTemplate
